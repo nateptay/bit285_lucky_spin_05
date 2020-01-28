6aaafedf018a68ce6c9990c501cd670146e09dcc
moscowkids <robtay@gmail.com>
2020-01-28 11:20:32 -0800

    Validate User's Data (Name, LuckyNumber)

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 //TODO: Add the library for Data Annotations
 namespace LuckySpin.Models
 {
@@ -6,10 +7,13 @@ namespace LuckySpin.Models
     {
         //TODO: Include two annotations for FirstName
         // one that makes it Required (no empty strings)
+        [Required]
         // and another that sets its Display prompt to "Your First Name"
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
         //TODO: Include one annotation for LuckyNumber
         // one that limits its Range between 1 and 9
+        [Range(1, 10, ErrorMessage = "Choose a Number")]
         public int LuckyNumber { get; set; }
     }
 }
